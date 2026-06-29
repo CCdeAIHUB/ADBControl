a9be092bee5afbbb0589da09c0420b7a01aa2688
CCdeAIHUB <21752490@qq.com>
2026-06-29 10:22:12 +0800

    Add Android companion app module

---
+++ android/companion-app/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.adbcontrol.companion"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.adbcontrol.companion"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
